b3481e96086fc72308e78b1c8ec6fe524b0a5882
dx120210209 <162584535+dx120210209@users.noreply.github.com>
2024-11-22 00:13:50 +0800

    Update app.rb

--- app.rb
+++ app.rb
@@ -11,7 +11,6 @@ puts `cat /github/workflow/event.json`
 
 
 EVENT_JSON = '/github/workflow/event.json'
-
 VIVO_BASE_URL = ENV['INPUT_BASE_URL']
 if VIVO_BASE_URL.nil?
   puts "INPUT_BASE_URL is blank"
@@ -28,31 +27,45 @@ if ACCESS_SECRET.nil?
   exit 1
 end
 puts "VIVO_BASE_URL: #{VIVO_BASE_URL} ACCESS_KEY: #{ACCESS_KEY} ACCESS_SECRET: #{ACCESS_SECRET}"
-
 def parse_event_json
-  return @event_json if defined? @event_json
-  @event_json = JSON.load_file(EVENT_JSON)
+  return @parse_event_json if defined? @parse_event_json
+
+  @parse_event_json = JSON.load_file(EVENT_JSON)
 end
 
 def commit_message
   parse_event_json['head_commit']['message']
 end
 
+def update_desc
+
+end
+
+def online_time
+  return @online_time if defined? @online_time
+
+  @online_time = commit_message.match(/\s*(\d{4}-\d{2}-\d{2}\s+\d{2}:\d{2}:\d{2})/)[1]
+end
+
 def parse_config_gradle
-  return @config_gradle if defined? @config_gradle
-  @config_gradle = File.read('config.gradle')
+  return @parse_config_gradle if defined? @parse_config_gradle
+
+  @parse_config_gradle = File.read('config.gradle')
 end
 
 def version_code
   return @version_code if defined? @version_code
+
   @version_code = parse_config_gradle.match(/VersionCode\s*:\s*(\d+)/)[1]
 end
 
 def version_name
   return @version_name if defined? @version_name
+
   @version_name = parse_config_gradle.match(/VersionName\s*:\s*"([^"]+)"/)[1]
 end
 
 puts "Commit Message: #{commit_message}"
 puts "VersionCode: #{version_code}"
 puts "VersionName: #{version_name}"
+puts "Online Time: #{online_time}"
